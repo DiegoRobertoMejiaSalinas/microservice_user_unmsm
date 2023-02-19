c9c2b7926aa36c2ee4cb95b21917489b7ac3c92b
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-02-18 19:20:17 -0500

    chore: Installed and integrated swagger and class-validators

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './utils/setup-swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger()
+  });
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  setupSwagger(app)
+
   await app.listen(3000);
 }
 bootstrap();
