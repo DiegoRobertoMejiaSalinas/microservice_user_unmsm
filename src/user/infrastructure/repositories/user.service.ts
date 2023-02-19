9fb7fdc524b551d4c224d7d29d4fcf02bb5ba3a9
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-02-18 19:24:09 -0500

    chore: CRUD implemented User

--- src/user/infrastructure/repositories/user.service.ts
+++ src/user/infrastructure/repositories/user.service.ts
@@ -1,28 +1,45 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { UserEntity } from "src/entities/user.entity";
-import { CreateUserDto } from "src/user/domain/dto/create-user.dto";
-import { UserRepository } from "./user.repository";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { CreateUserDto } from 'src/user/domain/dto/create-user.dto';
+import { UpdateUserDto } from 'src/user/domain/dto/update-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService{
-   constructor(@InjectRepository(UserEntity) private readonly _userRepository: UserRepository){}
-   
-   async listAllUsers(){
-    return await this._userRepository.find()
-   }
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly _userRepository: UserRepository,
+  ) {}
 
-   async findUserById(userId: number){
+  async listAllUsers() {
+    return await this._userRepository.find();
+  }
+
+  async findUserById(userId: number) {
     return await this._userRepository.findOne({
-        where: {
-            id: userId
-        }
-    })
-   }
+      where: {
+        id: userId,
+      },
+    });
+  }
 
-   async createUser(body: CreateUserDto){
+  async createUser(body: CreateUserDto) {
     return await this._userRepository.save({
-        ...body
-    })
-   }
+      ...body,
+    });
+  }
+
+  async updateUser(userId: number, body: UpdateUserDto) {
+    return await this._userRepository.save({
+      id: userId,
+      ...body,
+    });
+  }
+
+  async deleteUser(userId: number) {
+    return await this._userRepository.delete({
+      id: userId,
+    });
+  }
 }
